cf8d97fac446da4f1d14c15e14e544e50a9367f7
Xian Wu <wuxian94@pku.edu.cn>
2018-06-29 12:15:36 +0800

    还原sf

--- simpleflow/train.py
+++ simpleflow/train.py
@@ -38,27 +38,3 @@ class GradientDescentOptimizer(object):
                     var.output_value -= learning_rate*grad
 
         return MinimizationOperation()
-
-class GradientDescentOptimizerPS(object):
-    def __init__(self, host, port):
-        self.host = host
-        self.port = port
-        self.ps_client = WorkerClient(self.host, self.port)
-        self.ps_client.open()
-
-    def minimize(self, loss):
-        class MinimizationOperation(Operation):
-            def compute_output(self):
-                # Get gradient table.
-                grad_table = compute_gradients(loss)
-
-                # Iterate all trainable variables in graph.
-                for var in DEFAULT_GRAPH.trainable_variables:
-                    if var in grad_table:
-                        grad = grad_table[var]
-
-                    # Update its output value.
-                    var.output_value -= learning_rate*grad
-
-        return MinimizationOperation()
-
